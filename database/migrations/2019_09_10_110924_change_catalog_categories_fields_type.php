8aa8044da11f098733dc2bd05e83470dcc574c93
Victor <gordeev@flabers.com.ua>
2019-09-11 15:49:15 +0300

    fix

--- database/migrations/2019_09_10_110924_change_catalog_categories_fields_type.php
+++ database/migrations/2019_09_10_110924_change_catalog_categories_fields_type.php
@@ -14,10 +14,6 @@ class ChangeCatalogCategoriesFieldsType extends Migration
     public function up()
     {
         Schema::table('catalog_categories', function (Blueprint $table) {
-            $table->dropUnique('category_title');
-            $table->text('category_title')->change();
-            $table->dropUnique('slug');
-            $table->text('slug')->change();
             $table->text('meta_title')->nullable();
             $table->text('meta_description')->nullable();
             $table->text('meta_keywords')->nullable();
@@ -32,8 +28,6 @@ class ChangeCatalogCategoriesFieldsType extends Migration
     public function down()
     {
         Schema::table('catalog_categories', function (Blueprint $table) {
-            $table->string('slug')->change();
-            $table->unique('slug');
             $table->dropColumn('meta_title');
             $table->dropColumn('meta_description');
             $table->dropColumn('meta_keywords');
