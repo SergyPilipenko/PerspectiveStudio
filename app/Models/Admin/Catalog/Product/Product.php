0f2070952c8764101a05e330e4b9d3dc6f915485
Your Name <you@example.com>
2019-11-21 15:36:36 +0200

    fix products in admin panel

--- app/Models/Admin/Catalog/Product/Product.php
+++ app/Models/Admin/Catalog/Product/Product.php
@@ -20,6 +20,7 @@ use Partfix\QueryBuilder\Model\MysqlQueryBuilder;
 class Product extends Model implements ProductInterface
 {
     protected $fillable = ['type', 'attribute_family_id', 'quantity', 'article', 'parent_id', 'depends_quantity'];
+//    protected $table = 'products';
     protected $table = 'products_flat';
     public $priceFilter;
     /**
