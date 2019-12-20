651645689b063eda26d06fdc9f8faa4c225029b8
Ваше Имя <you@example.com>
2019-12-20 17:13:32 +0200

    return back old rubrics code

--- app/Http/Controllers/Frontend/RubricController.php
+++ app/Http/Controllers/Frontend/RubricController.php
@@ -30,33 +30,33 @@ class RubricController extends Controller
     public function index($slug)
     {
 //        СТАРЫЙ КОД
-//        $rubric = $this->rubric->where('slug', $slug)->with('groups.categories')
-        $rubric = $this->rubric->where('slug', $slug)->firstOrFail();
+        $rubric = $this->rubric->where('slug', $slug)->with('groups.categories')->firstOrFail();
+//        $rubric = $this->rubric->where('slug', $slug)->firstOrFail();
         $meta_tags = [
             'rubric_title' => $rubric->title
         ];
         $locale = app()->getLocale();
 
         /** НУЖНО УБРАТЬ ЭТО ИЗ КОНТРОЛЛЕРА */
-        $result = $this->builder->select('rubrics as r', [
-            'distinct rg.title as rubric_title',
-            'rg.id as rubric_id',
-            'c.id as category_id',
-            'json_unquote(json_extract(c.slug, \'$."'.$locale.'"\')) as slug',
-            'json_unquote(json_extract(c.category_title, \'$."'.$locale.'"\')) as category_title',
-            'c.image',
-            'c.type'
-        ])->join('rubric_groups as rg', 'r.id', 'rg.rubric_id')
-            ->join('rubric_group_mappings as rgm','rg.id','rgm.rubric_group_id')
-            ->join('catalog_categories as c','rgm.category_id','c.id')
-            ->join('category_distinct_passanger_car_trees as cdp', 'c.id', 'cdp.category_id')
-            ->join('distinct_passanger_car_trees as dp','cdp.distinct_pct_id','dp.id')
-            ->join(env('DB_TECDOC_DATABASE').'.article_tree as art','dp.passanger_car_trees_id','art.nodeid')
-            ->join('prices as p', 'art.article_number_id', 'p.article_id')
-            ->where('r.slug', $slug)
-            ->where('p.price','0', '>')
-            ->getArrayResult();
-        $groups = $this->formatData($result);
+//        $result = $this->builder->select('rubrics as r', [
+//            'distinct rg.title as rubric_title',
+//            'rg.id as rubric_id',
+//            'c.id as category_id',
+//            'json_unquote(json_extract(c.slug, \'$."'.$locale.'"\')) as slug',
+//            'json_unquote(json_extract(c.category_title, \'$."'.$locale.'"\')) as category_title',
+//            'c.image',
+//            'c.type'
+//        ])->join('rubric_groups as rg', 'r.id', 'rg.rubric_id')
+//            ->join('rubric_group_mappings as rgm','rg.id','rgm.rubric_group_id')
+//            ->join('catalog_categories as c','rgm.category_id','c.id')
+//            ->join('category_distinct_passanger_car_trees as cdp', 'c.id', 'cdp.category_id')
+//            ->join('distinct_passanger_car_trees as dp','cdp.distinct_pct_id','dp.id')
+//            ->join(env('DB_TECDOC_DATABASE').'.article_tree as art','dp.passanger_car_trees_id','art.nodeid')
+//            ->join('prices as p', 'art.article_number_id', 'p.article_id')
+//            ->where('r.slug', $slug)
+//            ->where('p.price','0', '>')
+//            ->getArrayResult();
+//        $groups = $this->formatData($result);
         $activeCar = app('App\Classes\Garage')->getGarage()->activeCar ?? null;
         $viewedProducts = $this->viewedProducts->getViewedProducts();
 
