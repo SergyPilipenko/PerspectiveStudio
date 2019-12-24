d2cc9cb889cdc994080e46c199b54fa12ff247b6
flabers <studio@flabers.com.ua>
2019-12-24 16:23:54 +0000

    Revert "Merge branch 'development' into 'master'"
    This reverts merge request !199

--- app/Models/Content/Rubric/Rubric.php
+++ app/Models/Content/Rubric/Rubric.php
@@ -4,9 +4,7 @@
 namespace App\Models\Content\Rubric;
 
 use App\Models\Catalog\Category;
-use App\Search\Indexers\CategoriesIndexer;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\File;
 
 class Rubric extends Model
 {
@@ -16,23 +14,6 @@ class Rubric extends Model
     public function groups()
     {
         return $this->hasMany(RubricGroup::class, 'rubric_id', 'id')->orderBy('position', 'ASC');
-    }
-    protected static function boot()
-    {
-        parent::boot();
-        if(env('APP_DEBUG')) {
-
-            static::created(function ($rubric) {
-                File::put(public_path('SomethingChanged.txt'), 'changed');
-            });
-            static::updated(function ($rubric) {
-                File::put(public_path('SomethingChanged.txt'), 'changed');
-            });
-            static::deleted(function ($rubric) {
-                File::put(public_path('SomethingChanged.txt'), 'changed');
-            });
-
-        }
     }
 
     public function scopeMenu($query)
